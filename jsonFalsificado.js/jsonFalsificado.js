7eeec1e9e54df70ea9175ed074172db09c241186
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-07-01 11:54:11 +0200

    Add files via upload

---
+++ jsonFalsificado.js/jsonFalsificado.js
@@ -0,0 +1,29 @@
+const articles = [
+    {
+        id: 1,
+        titular: "Artículo 1"
+        contenido: "Contenido del artículo 1."
+    },
+    {
+        id: 2,
+        titular: "Artículo 2",
+        contenido: "Contenido del artículo 2."
+    },
+    {
+        id: 3,
+        titular: "Artículo 3",
+        contenido: "Contenido del artículo 3."
+    },
+    {
+        id: 4,
+        titular: "Artículo 4",
+        contenido: "Contenido del artículo 4."
+    },
+    {
+        id: 5,
+        titular: "Artículo 5",
+        contenido: "Contenido del artículo 5."
+    }
+];
+
+console.log(articles);
